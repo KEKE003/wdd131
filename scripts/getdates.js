1e4a388c7221bb27e346e65e9c81ec16be68bd2b
KEKE003 <knhlapo@byupathway.edu>
2026-07-04 23:00:15 +0200

    Updated the index.html footer tag and the .js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,23 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 
-
-
-
-
-
-
-
-
-
 const currentYearSpan = document.getElementById("current-year");
 if (currentYearSpan) {
     currentYearSpan.textContent = new Date().getFullYear();
 }
 
-const lastModifiedParagraph = document.getElementById("last-modified");
-if (lastModifiedParagraph) {
-    lastModifiedParagraph.textContent =  `Last Modification: ${document.lastModified}`;
+const lastModifiedSpan = document.getElementById("last-modified");
+if (lastModifiedSpan) {
+    lastModifiedSpan.textContent =  `Last Modification: ${document.lastModified}`;
 }
 
 });
