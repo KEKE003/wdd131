ba129eff326c56bceb11c48790002995ee87577e
KEKE003 <knhlapo@byupathway.edu>
2026-07-04 21:54:26 +0200

    Updated the HTML

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,9 +1,21 @@
-<script>
-    // Get the current year
-    const currentYear = new Date().getFullYear();
-    document.getElementById("current-year").textContent = currentYear;
 
-    // Get the last modified date
-    const lastModified = document.lastModified;
-    document.getElementById("last-modified").innerHTML = document.lastModified;
-</script>
+
+
+
+
+
+
+
+
+
+
+
+const currentYear = new Date().getFullYear();
+document.getElementById("current-year").innerHTML = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById("last-modified").innerHTML = document.lastModified;
+
+
+
+
