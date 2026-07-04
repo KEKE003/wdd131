719f2c987ffa9bee3351afadca5509271c55aa6e
KEKE003 <knhlapo@byupathway.edu>
2026-07-04 22:52:41 +0200

    Updated scripts

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -15,9 +15,9 @@ if (currentYearSpan) {
     currentYearSpan.textContent = new Date().getFullYear();
 }
 
-const lastModifiedSpan = document.getElementById("last-modified");
-if (lastModifiedSpan) {
-    lastModifiedSpan.textContent =  `Last Modification: ${document.lastModified}`;
+const lastModifiedParagraph = document.getElementById("last-modified");
+if (lastModifiedParagraph) {
+    lastModifiedParagraph.textContent =  `Last Modification: ${document.lastModified}`;
 }
 
 });
